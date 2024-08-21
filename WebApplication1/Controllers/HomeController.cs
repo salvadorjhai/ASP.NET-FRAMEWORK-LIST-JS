f9594134e08f9838525dd08ee0146832d94b9db8
salvadorjhai <salvadorjhai@outlook.com>
2024-08-21 10:50:00 +0800

    added another sample

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace WebApplication1.Controllers
 {
@@ -10,6 +11,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            var s = FormsAuthentication.Decrypt("75EE059188E56C66C74248F664C59CF01867877376A1FD099BDE4127FA118FE7AC68C7CD2CE4A9C8490160380BADB49DC3C5054F7E5DBC1F2C30898A45EFDB8676ADCB0FF9EAD30FD3DAD37DEB45041D");
             return View();
         }
 
@@ -24,6 +26,12 @@ namespace WebApplication1.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Requirements()
+        {
+            ViewBag.Message = "Requirements";
             return View();
         }
     }
---
+++ WebApplication1/Models/MemberModelAttachments.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class MemberModelAttachments
+    {
+
+        public int id { get; set; }
+
+        [Required]
+        public int memberid { get; set; }
+
+        [MaxLength(255)]
+        public string title { get; set; }
+
+        [MaxLength(255)]
+        public string description { get; set; }
+
+        [MaxLength(255)]
+        public string original_filename { get; set; }
+
+        [MaxLength(255)]
+        public string hashed_filename { get; set; }
+
+
+        public int statuslvl { get; set; }
+
+        public int madebyid { get; set; }
+
+        public DateTime? madedate { get; set; }
+
+        public DateTime? lastupdated { get; set; }
+
+        public int updatedbyid { get; set; }
+
+    }
+}
